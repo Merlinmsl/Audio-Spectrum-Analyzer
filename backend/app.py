f1900a99a678488bdad2d9541f8be7c4d4daf71c
Merlinmsl <minsarasammanitha@gmail.com>
2026-07-10 14:03:33 +0530

    Change to accept JSON instead of file upload

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,3 @@
-import os
-import tempfile
 import flask import Flask, request, jsonify
 from flask_cors import  CORS
 from analyzer import analyze_audio
@@ -9,28 +7,16 @@ CORS(app) # Allow frontend (different port) to call this API
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    # check if file was sent
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file uploaded!'}), 400
-    
-    file = request.files['file']
-    
-    if file.filename == '':
-        return jsonify({'error': 'No file selected!'}), 400
-    
-    # Save to a tempory file (librosa needs a path, not file object)
-    with tempfile.NamedTemporaryFiles(delete=False, suffix='.mp3') as temp:
-        file.save(temp.name)
-        tmp_path = tmp.name
-        
+    data = request.get_json()
+    if not data or 'samples' not in data or 'sample_rate' not in data:
+        return jsonify({'error': 'Missing samples or sampleRate'}), 400
     try:
-        frequencies, magnitude = analyze_audio(tmp_path)
-        return jsonify({'frequencies': frequencies, 'magnitude': magnitudes})
+        frequencies, magnitudes = analyze_audio(data['samples'], data['sample_rate'])
+        
+        return jsonify({'frequencies': frequencies, 'magnitudes': magnitudes})
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    finally:
-        os.unlink(tmp_path) # Delete temp file
         
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
